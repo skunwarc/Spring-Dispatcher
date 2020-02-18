234415de2709db772adb16082b68de48ea17ebc4
Sagar Kunwar Chhetri <sagar_kunwar@outlook.com>
2020-02-18 16:09:50 -0500

    first commit

---
+++ src/main/java/com/abc/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package com.abc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value="hello", method = RequestMethod.GET)
+	public void helloDisplay() {
+		System.out.println("Print hello from method helloDisplay");
+	}
+	
+}

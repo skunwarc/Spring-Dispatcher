b8b758cbf520fac0ce7dd1844c4309b1bd13cfc9
Sagar Kunwar Chhetri <sagar_kunwar@outlook.com>
2020-02-19 16:27:00 -0500

    ModelAndView and DispatcherServlet

--- src/main/java/com/abc/controllers/HelloController.java
+++ src/main/java/com/abc/controllers/HelloController.java
@@ -3,14 +3,27 @@ package com.abc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.abc.models.Customer;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping(value="hello", method = RequestMethod.GET)
-	public String helloDisplay() {
-		System.out.println("Print hello from method helloDisplay");
-		return "hello-page";
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public ModelAndView helloDisplay() {
+		
+		ModelAndView modelAndView = new ModelAndView();
+
+		modelAndView.setViewName("hello");
+		modelAndView.addObject("location", "fairafx");
+		
+		Customer customer = new Customer();
+		customer.setEmail("abc@gmail.com");
+		customer.setName("Aman");
+		modelAndView.addObject("customer-info", customer);
+		return modelAndView;
+
 	}
-	
+
 }
---
+++ src/main/webapp/pages/hello.jsp
@@ -0,0 +1,16 @@
+<html>
+<body>
+	<table>
+		<tr>
+			<td>Name</td>
+			<td>"${customer-info.name}"</td>
+		</tr>
+		
+		<tr>
+			<td>Email</td>
+			<td>"${customer-info.email}"</td>
+		</tr>
+	</table>
+
+</body>
+</html>
